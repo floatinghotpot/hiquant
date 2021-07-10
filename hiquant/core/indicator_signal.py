712f9bd3ad960c58f657c59183fc39b9bd0fde25
Liming Xie <liming.xie@gmail.com>
2021-07-11 04:38:11 +0800

    rewrite signal and long logic

--- hiquant/core/indicator_signal.py
+++ hiquant/core/indicator_signal.py
@@ -2,20 +2,54 @@
 
 import pandas as pd
 
-def signal_to_long(signal, long_value = 1, short_value = 0):
-    trend = [0 for i in range(signal.size)]
-    for i in range(0, signal.size):
+def signal_no_dup(signal):
+    sig = signal[0]
+    for i in range(1, signal.size):
         s = signal.iloc[i]
-        if s > 0:
-            trend[i] = long_value
-        elif s < 0:
-            trend[i] = short_value
-        elif i > 0:
-            trend[i] = trend[i-1]
-    return pd.Series(trend, index=signal.index)
+        if s == 0:
+            continue
+        elif s == sig: # same
+            signal.iloc[i] = 0
+        else: # different, change compare target
+            sig = s
 
-def long_to_signal(trend):
-    return trend.diff(1).fillna(0)
+    return signal
+
+def signal_to_long(signal, time_factor = 1.0):
+    # remove dup signal to avoid cumsum exceed 1
+    signal = signal_no_dup(signal)
+
+    # remove first trade signal if it's sell signal
+    for i in range(signal.size):
+        s = signal.iloc[i]
+        if s == 0:
+            continue
+        elif s > 0:
+            break
+        else:
+            signal.iloc[i] = 0
+            break
+
+    # cumsum, valid pos value should be either 0 or 1
+    long_pos = signal.cumsum().fillna(0)
+
+    if time_factor < 0 or time_factor > 1.0:
+        raise ValueError('please make sure 0 <= long_factor <= 1')
+
+    # if buy/sell in time, we can earn more and lose less
+    long_factor = 1.0 - time_factor
+    short_factor = time_factor
+    pos = 0
+    for i in range(long_pos.size):
+        p = long_pos.iloc[i]
+        if p > pos: # first long pos, it's a buy
+            long_pos.iloc[i] = long_factor
+            pos = p
+        elif p < pos: # first short pos, it's a sell
+            long_pos.iloc[i] = short_factor
+            pos = p
+
+    return long_pos
 
 _registered_signal_indicators = {
     # id: {
@@ -39,8 +73,8 @@ def register_signal_indicator(id, func, cols, label, ind_type):
         'type': ind_type,
         'label': label,
     }
- 
-def gen_indicator_signal(df, indicators, inplace=False):
+
+def gen_indicator_signal(df, indicators, time_factor = 1.0, inplace=False):
     signal = pd.Series(0, index=df.index)
     for k in indicators:
         if k in _registered_signal_indicators:
@@ -53,5 +87,4 @@ def gen_indicator_signal(df, indicators, inplace=False):
         else:
             print(k, 'not found in registered indicators')
 
-    long = signal_to_long(signal)
-    return long.diff(1).fillna(0)
+    return signal_no_dup(signal)
