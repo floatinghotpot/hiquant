4b832411a66034c309c50e8e9a175af7dbc3e773
Liming Xie <liming.xie@gmail.com>
2021-07-20 09:15:41 +0800

    refactoring finance cli

--- hiquant/cli/cli_finance.py
+++ hiquant/cli/cli_finance.py
@@ -8,64 +8,21 @@ import tabulate as tb
 from ..core.data_cache import get_finance_indicator_df
 from ..utils import sort_with_options, filter_with_options, date_from_str, datetime_today
 
-def cli_finance(params, options):
-    syntax_tips = '''Syntax:
-    __argv0__ finance <action> <symbols | stockpool.csv | all> [options]
+def cli_finance_update(params, options):
+    if len(params) > 0:
+        check_date = date_from_str(params[0])
+    else:
+        # first day of this quarter
+        today = datetime_today()
+        quarter_first_month = (today.month -1) // 3 * 3 + 1
+        check_date = dt.datetime(today.year, quarter_first_month, 1)
+        print('checking date:', check_date)
 
-Action:
-    update ......................... download finance reports and update finance indicators
-    view ........................... view finance indicators with filters
+    df = get_finance_indicator_df(check_date= check_date)
+    print(df)
 
-Symbols:
-    <symbols> ...................... stock symbol list, like 600036 000002
-    <stockpool.csv> ................ stock pool csv file
-    all ............................ all symbols
-
-Options:
-    -sortby=<col> .................. sort by the column
-
-    -ipo_years=<from>-<to> ......... IPO years between <from> to <to>
-    -earn_ttm=<from>-<to> .......... annual earn between <from> to <to>
-    -roe=<from>-<to> ............... ROE between <from> to <to>
-    -grow_rate=<from>-<to> ......... average yearly grow rate between <from> to <to>
-    -3yr_grow_rate=<from>-<to> ..... recent 3 year grow rate between <from> to <to>
-
-    -tab ........................... show data in table format
-
-    -out=<out.csv> ................. export selected stocks into <out.csv> file
-
-Example:
-    __argv0__ finance show 600036 000002 600276
-    __argv0__ finance show my-stocks.csv
-    __argv0__ finance show all -ipo_years=1- -earn_ttm=1.0- -roe=0.15- -3yr_grow_rate=0.15- -sortby=roe -out=good_stock.csv
-'''.replace('__argv0__',os.path.basename(sys.argv[0]))
-
-    if (len(params) == 0) or (params[0] == 'help'):
-        print(syntax_tips)
-        return
-
-    action = params[0]
-    params = params[1:]
-
-    if action not in ['update', 'view', 'show', 'filter']:
-        print('\nError: invalid action: ', action)
-        return
-
-    if action == 'update':
-        if len(params) > 0:
-            check_date = date_from_str(params[0])
-        else:
-            # first day of this quarter
-            today = datetime_today()
-            quarter_first_month = (today.month -1) // 3 * 3 + 1
-            check_date = dt.datetime(today.year, quarter_first_month, 1)
-            print('checking date:', check_date)
-
-        df = get_finance_indicator_df(check_date= check_date)
-        print(df)
-        return
-
-    if params[0] == 'all':
+def cli_finance_view(params, options):
+    if (len(params) == 0) or (params[0] == 'all'):
         df = get_finance_indicator_df()
     else:
         if params[0].endswith('.csv'):
@@ -110,3 +67,55 @@ Example:
         print(df)
 
     print('')
+
+def cli_finance(params, options):
+    syntax_tips = '''Syntax:
+    __argv0__ finance <action> <symbols | stockpool.csv | all> [options]
+
+Action:
+    update ......................... download finance reports and update finance indicators
+    view ........................... view finance indicators with filters
+
+Symbols:
+    <symbols> ...................... stock symbol list, like 600036 000002
+    <stockpool.csv> ................ stock pool csv file
+    all ............................ all symbols
+
+Options:
+    -sortby=<col> .................. sort by the column
+
+    -ipo_years=<from>-<to> ......... IPO years between <from> to <to>
+    -earn_ttm=<from>-<to> .......... annual earn between <from> to <to>
+    -roe=<from>-<to> ............... ROE between <from> to <to>
+    -grow_rate=<from>-<to> ......... average yearly grow rate between <from> to <to>
+    -3yr_grow_rate=<from>-<to> ..... recent 3 year grow rate between <from> to <to>
+
+    -tab ........................... show data in table format
+
+    -out=<out.csv> ................. export selected stocks into <out.csv> file
+
+Example:
+    __argv0__ finance view 600036 000002 600276
+    __argv0__ finance view my-stocks.csv
+    __argv0__ finance view all -ipo_years=1- -earn_ttm=1.0- -roe=0.15- -3yr_grow_rate=0.15- -sortby=roe -out=good_stock.csv
+'''.replace('__argv0__',os.path.basename(sys.argv[0]))
+
+    if (len(params) == 0) or (params[0] == 'help'):
+        print(syntax_tips)
+        return
+
+    action = params[0]
+    params = params[1:]
+
+    if action not in ['update', 'view', 'show', 'filter']:
+        print('\nError: invalid action: ', action)
+        return
+
+    if action == 'update':
+        cli_finance_update(params, options)
+        return
+    elif action in ['view', 'show', 'filter']:
+        cli_finance_view(params, options)
+    else:
+        print('invalid action:', action)
+        print(syntax_tips)
