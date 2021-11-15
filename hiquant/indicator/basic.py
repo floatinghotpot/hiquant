00a984c9fd5e6991589adc3aaef1ba31f25176eb
Liming Xie <liming.xie@gmail.com>
2021-11-15 23:59:01 +0800

    refactoring

--- hiquant/indicator/basic.py
+++ hiquant/indicator/basic.py
@@ -102,3 +102,58 @@ def PEAK(data):
     # if >= 3, large than near 3 or 4 values, we regard it a peak
     return data // 3
 
+def is_percent(x: int or float) -> bool:
+    if isinstance(x, (int, float)):
+        return x is not None and x >= 0 and x <= 100
+    return False
+
+def INCREASING(close, length=1, strict=False, asint=True, percent=None, drift=1):
+    # Validate Arguments
+    length = int(length) if length and length > 0 else 1
+    strict = strict if isinstance(strict, bool) else False
+    asint = asint if isinstance(asint, bool) else True
+    percent = float(percent) if is_percent(percent) else False
+
+    if close is None: return
+
+    # Calculate Result
+    close_ = (1 + 0.01 * percent) * close if percent else close
+    if strict:
+        # Returns value as float64? Have to cast to bool
+        increasing = close > close_.shift(drift)
+        for x in range(3, length + 1):
+            increasing = increasing & (close.shift(x - (drift + 1)) > close_.shift(x - drift))
+
+        increasing.fillna(0, inplace=True)
+        increasing = increasing.astype(bool)
+    else:
+        increasing = close_.diff(length) > 0
+
+    if asint:
+        increasing = increasing.astype(int)
+
+    return increasing
+
+def DECREASING(close, length=1, strict=False, asint=True, percent=0, drift=1):
+    length = int(length) if length and length > 0 else 1
+    strict = strict if isinstance(strict, bool) else False
+    asint = asint if isinstance(asint, bool) else True
+    percent = float(percent) if percent else False
+
+    # Calculate Result
+    close_ = (1 - 0.01 * percent) * close if percent else close
+    if strict:
+        # Returns value as float64? Have to cast to bool
+        decreasing = close < close_.shift(drift)
+        for x in range(3, length + 1):
+            decreasing = decreasing & (close.shift(x - (drift + 1)) < close_.shift(x - drift))
+
+        decreasing.fillna(0, inplace=True)
+        decreasing = decreasing.astype(bool)
+    else:
+        decreasing = close_.diff(length) < 0
+
+    if asint:
+        decreasing = decreasing.astype(int)
+
+    return decreasing
